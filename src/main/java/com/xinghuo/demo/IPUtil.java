76293860397aecd92d3cc7e1dfb4f40b85205f0e
yong472727322 <472727322@qq.com>
2018-10-24 16:47:31 +0800

    修改服务器地址

--- src/main/java/com/xinghuo/demo/IPUtil.java
+++ src/main/java/com/xinghuo/demo/IPUtil.java
@@ -44,6 +44,8 @@ public class IPUtil  implements CommandLineRunner {
 
     @Value("${server.ip}")
     private String server;
+    @Value("${vps.server.port}")
+    private int port;
     @Value("${vps.ip}")
     private String vps;
 
@@ -130,7 +132,7 @@ public class IPUtil  implements CommandLineRunner {
         if(!b){
             if(vilidIP(currentHost)){
                 log.info("当前IP[{}]和发送给服务器的IP[{}]不一致，发送给服务器。",currentHost,host);
-                sendGet("http://"+server+":1819/amazon/recordIP?ip=" + currentHost + "&vps="+vps);
+                sendGet("http://"+server+":"+port+"/amazon/recordIP?ip=" + currentHost + "&vps="+vps);
                 if(0 == failCount){
                     host = currentHost;
                 }
@@ -255,7 +257,7 @@ public class IPUtil  implements CommandLineRunner {
 
     private void sendHost(String newIp) {
         log.info("send new ip [{}] to server",newIp);
-        sendGet("http://"+server+":1819/amazon/saveIP?port=4431&password=password&account="+vps+"&ip=" + newIp+"&vps="+vps);
+        sendGet("http://"+server+":"+port+"/amazon/saveIP?port=4431&password=password&account="+vps+"&ip=" + newIp+"&vps="+vps);
     }
 
     private boolean flag = false;
@@ -276,7 +278,7 @@ public class IPUtil  implements CommandLineRunner {
     }
 
     public void sendVpsState(boolean state){
-        String url = "http://"+server+":1819/amazon/recordVpsState?vps="+vps+"&state=" + state;
+        String url = "http://"+server+":"+port+"/amazon/recordVpsState?vps="+vps+"&state=" + state;
         sendGet(url);
     }
 }
